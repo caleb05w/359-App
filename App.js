ceda5658fe8f1653d7c8b6e3cbe0bea9aa71088d
Caleb Wu <caleb05w@gmail.com>
2025-11-01 14:53:35 -0700

    my first commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import { NavigationContainer } from "@react-native/native";
+import { NavigationContainer } from "@react-navigation/native";
+
+import HomeScreen from "./src/screens/HomeScreen.js";
+import WelcomeScreen from "./src/screens/WelcomeScreen.js";
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <NavigationContainer>
+        <Stack.Navigator intialRouteName="Home">
+          <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Home Screen" }}
+          />
+
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{ title: "Welcome Screen" }}
+          />
+        </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
