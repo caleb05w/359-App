d63cdbc06e55626f89ead59938ef611a04074aa7
Kaitlynrks <kaitlyn_rukus@sfu.ca>
2025-11-04 13:09:17 -0800

    Update LoginScreen.js

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,4 +1,6 @@
-import {View, Text} from 'react-native'
+import {useState, useEffect} from 'react';
+import { Button, View, Text, TextInput, TouchableOpacity, StyleSheet, Alert} from 'react-native'
+import { saveUserPrefs, loadUserPrefs, removeUserPrefs } from '../utils/storage';
 
 export default function LoginScreen(navigation) {
     return (
